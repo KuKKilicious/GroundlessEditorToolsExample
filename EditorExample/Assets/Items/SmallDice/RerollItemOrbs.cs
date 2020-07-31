56fddf70c3a5078b1d294b82b5942babbab15747
KuKKi <clouseyda@yahoo.de>
2020-07-31 17:33:37 +0200

    feat: Import icons and create example items

---
+++ EditorExample/Assets/Items/SmallDice/RerollItemOrbs.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Example Custom Behaviour
+public class RerollItemOrbs : MonoBehaviour
+{
+   // Get Called
+
+   //Inquire Platform Manager
+
+   //Request respawn of item orbs
+}
